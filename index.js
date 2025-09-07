ac50623697437220d6ae56f190dfb804b08c5d55
zubdotexe <64923600+zubdotexe@users.noreply.github.com>
2025-09-07 23:36:10 +0600

    implemented dynamic tree categories panel

---
+++ index.js
@@ -0,0 +1,39 @@
+const categories = document.getElementById("categories");
+
+const url = "https://openapi.programming-hero.com/api/categories";
+fetch(url)
+.then(res => res.json())
+.then(catArr => {
+    let btnContainer = document.createElement("div");
+    btnContainer.innerHTML = `
+        <div>
+            <button id="0" class="w-full cursor-pointer hover:bg-[#15803D] hover:text-white p-2 text-left rounded-sm" href="">All Plants</button>
+        </div>
+    `
+    catArr["categories"].forEach(catObj => {
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = `
+            <button id="${catObj["id"]}" class="w-full cursor-pointer hover:bg-[#15803D] text-[#1F2937] hover:text-white p-2 text-left rounded-sm" href="">${catObj["category_name"]}</button>
+        `
+
+        btnContainer.append(tempDiv);
+    });
+
+    categories.innerHTML = btnContainer.innerHTML;
+})
+
+const activateBtnCat = (btnCat) => {
+    categories.querySelectorAll("div").forEach(catDiv => {
+            console.log(catDiv.querySelector("button"));
+            catDiv.querySelector("button").classList.remove("btnActiveCat");
+        });
+
+        btnCat.classList.add("btnActiveCat");
+}
+
+categories.addEventListener("click", (e) => {
+    const btnCat = e.target.closest("button");
+    if(btnCat) {
+        activateBtnCat(btnCat);
+    }
+})
